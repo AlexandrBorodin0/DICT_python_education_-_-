84caca78389ee157b44090c7cc62b0444ac090b7
Бородин Александр <crazymegahell064@gmail.com>
2022-02-01 01:24:25 +0200

    ChatBot 3-st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,5 +1,14 @@
 print("Hello! My name is Hagrit.")
 print("I was created in 2021.")
+
 print("Please, remind me your name.")
 name = input("> ")
 print(f"What a great name you have, {name}!")
+
+print("Let me guess your age.")
+print("Enter remainders of dividing your age by 3,5 and 7.")
+age_1 = int(input("> "))
+age_2 = int(input("> "))
+age_3 = int(input("> "))
+age = (age_1 * 70 + age_2 * 21 + age_3 * 15) % 105
+print(f"Your age is {age}; that's good time to start programming!")
