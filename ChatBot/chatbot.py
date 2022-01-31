82c60c1c500b692139a3ccb8a029fbccb81c13b0
Бородин Александр <crazymegahell064@gmail.com>
2022-02-01 01:22:46 +0200

    ChatBot 2-st stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,5 @@
-print("Hello! My name is Hagrit")
-print("I was created in 2021")
+print("Hello! My name is Hagrit.")
+print("I was created in 2021.")
+print("Please, remind me your name.")
+name = input("> ")
+print(f"What a great name you have, {name}!")
