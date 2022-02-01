6f5d8b50c24fa90b397cd78690bda3d4630ea9cb
Бородин Александр <crazymegahell064@gmail.com>
2022-02-01 14:31:09 +0200

    Hangman 4-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,7 +4,9 @@ import random
 print("HANGMAN")
 lists = ["python", "java", "javascript", "php"]
 win_word = random.choice(lists)
-user = input("Guess the word:> ")
+clue_spaces = "-" * (len(win_word) - 3)
+clue_letters = "".join(list(win_word)[0:3])
+user = input(f"Guess the word {clue_letters + clue_spaces}:> ")
 if user == win_word:
     print("You survived!")
 else:
