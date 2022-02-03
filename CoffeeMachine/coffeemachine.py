72b5ef83c5a44ff3e488cfc8a0f93d39eb3feb02
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 00:36:59 +0200

    CoffeeMachine 1-st stage

---
+++ CoffeeMachine/coffeemachine.py
@@ -0,0 +1,6 @@
+print("Starting to make a coffee")
+print("Grinding coffee beans")
+print("Boiling water")
+print("Mixing boiled water with crushed coffee beans")
+print("Pouring coffee into the cup")
+print("Coffee is ready!")
