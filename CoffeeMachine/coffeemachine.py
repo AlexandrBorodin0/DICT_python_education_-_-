d202cad763ba8d5de229042558159f92fa18016b
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 00:38:09 +0200

    CoffeeMachine 3-st stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -1,9 +1,34 @@
-print("Write how many cups of coffee you will need: ")
-cups = int(input("> "))
-water = cups * 200
-milk = cups * 50
-beans = cups * 15
-print(f"For {cups} cups of coffee you will need:")
-print(f"{water} ml of water")
-print(f"{milk} ml of milk")
-print(f"{beans} g of coffee beans")
+water = 200
+milk = 50
+beans = 15
+
+print("Write how many ml water the coffee machine has:")
+user_water = int(input("> "))
+print("Write how many ml milk the coffee machine has:")
+user_milk = int(input("> "))
+print("Write how many grams of coffee beans the coffee machine has:")
+user_beans = int(input("> "))
+print("Write how many cups of coffee you  will need:")
+user_cups = int(input("> "))
+
+
+def main():
+    many_w = user_water // water
+    many_m = user_milk // milk
+    many_b = user_beans // beans
+    if many_w * many_m * many_b == 0:
+        cups = 0
+    else:
+        cups = min(many_w, many_m, many_b)
+
+    difference_cups = cups - user_cups
+
+    if user_cups == cups:
+        print("Yes, I can make that amount of coffee")
+    elif user_cups >= cups:
+        print(f"No, I can make only {cups} cups of coffee")
+    else:
+        print(f"Yes, I can make that amount of coffee (and even {difference_cups} more than that)")
+
+
+main()
