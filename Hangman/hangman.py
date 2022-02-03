dde5c80c951cef75e1b891b36b20a30bb0035a91
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 00:23:56 +0200

    Hangman 5-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,11 +3,23 @@ import random
 
 print("HANGMAN")
 lists = ["python", "java", "javascript", "php"]
-win_word = random.choice(lists)
-clue_spaces = "-" * (len(win_word) - 3)
-clue_letters = "".join(list(win_word)[0:3])
-user = input(f"Guess the word {clue_letters + clue_spaces}:> ")
-if user == win_word:
-    print("You survived!")
+win_word = list(random.choice(lists))
+clue = list("-" * len(win_word))
+print(''.join(clue))
+for i in range(8):
+    user_letter = input("Input a letter: ")
+    if user_letter in win_word:
+        if win_word.count(user_letter) >= 2:
+            index = win_word.index(user_letter)
+            clue[index] = user_letter
+            win_word[index] = "-"
+        index = win_word.index(user_letter)
+        clue[index] = user_letter
+    print(''.join(clue))
+if win_word == clue:
+    pass
 else:
-    print("You lost!")
+    print("That letter doesn't appear in the word")
+
+print("\nThanks or playing!")
+print("We'll se how well you did in next stage")
