5bf6cbca25bc86405cf4b994fce172387122ff14
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 00:38:41 +0200

    CoffeeMachine 4-st stage

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -1,34 +1,75 @@
-water = 200
-milk = 50
-beans = 15
+water = 400
+milk = 540
+beans = 120
+cups = 9
+money = 550
 
-print("Write how many ml water the coffee machine has:")
-user_water = int(input("> "))
-print("Write how many ml milk the coffee machine has:")
-user_milk = int(input("> "))
-print("Write how many grams of coffee beans the coffee machine has:")
-user_beans = int(input("> "))
-print("Write how many cups of coffee you  will need:")
-user_cups = int(input("> "))
+
+def espresso():
+    water_espresso = water - 250
+    milk_espresso = milk
+    beans_espresso = beans - 16
+    cups_espresso = cups - 1
+    money_espresso = money + 4
+    init(water_espresso, milk_espresso, beans_espresso, cups_espresso, money_espresso)
+
+
+def latte():
+    water_latte = water - 350
+    milk_latte = milk - 75
+    beans_latte = beans - 20
+    cups_latte = cups - 1
+    money_latte = money + 7
+    init(water_latte, milk_latte, beans_latte, cups_latte, money_latte)
+
+
+def cappuccino():
+    water_cappuccino = water - 200
+    milk_cappuccino = milk - 100
+    beans_cappuccino = beans - 12
+    cups_cappuccino = cups - 1
+    money_cappuccino = money + 6
+    init(water_cappuccino, milk_cappuccino, beans_cappuccino, cups_cappuccino, money_cappuccino)
+
+
+def fill():
+    wat = (input("Write how many ml of water you want to add:")) + water
+    mil = (input("Write how many ml of milk you want to add:")) + milk
+    bean = (input("Write how many grams of coffee beans you want to add:")) + beans
+    cup = (input("Write how disposable coffee cups you want to add:")) + cups
+    init(wat, mil, bean, cup, money)
+
+
+def take():
+    print(f"I gave you {money}")
+    money_take = 0
+    init(water, milk, beans, cups, money_take)
+
+
+def init(w, m, b, c, mon):
+    print(f"The coffee machine has:\n{w} of water")
+    print(f"{m} of milk\n{b} of coffee beans")
+    print(f"{c} of disposable cups\n{mon} of money")
 
 
 def main():
-    many_w = user_water // water
-    many_m = user_milk // milk
-    many_b = user_beans // beans
-    if many_w * many_m * many_b == 0:
-        cups = 0
+    user = input("Write action (buy, fill, take):\n> ")
+    if user == "buy":
+        buy = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:"))
+        if buy == 1:
+            espresso()
+        elif buy == 2:
+            latte()
+        elif buy == 3:
+            cappuccino()
+        else:
+            print("Invalid input")
+    elif user == "fill":
+        fill()
+    elif user == "take":
+        take()
     else:
-        cups = min(many_w, many_m, many_b)
-
-    difference_cups = cups - user_cups
-
-    if user_cups == cups:
-        print("Yes, I can make that amount of coffee")
-    elif user_cups >= cups:
-        print(f"No, I can make only {cups} cups of coffee")
-    else:
-        print(f"Yes, I can make that amount of coffee (and even {difference_cups} more than that)")
+        print("Invalid input")
 
 
 main()
