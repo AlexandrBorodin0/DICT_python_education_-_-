f47db253de024a49d6bb9cfc614badaf916196cb
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 00:26:12 +0200

    Hangman 8-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -8,32 +8,50 @@ win_word = list(word)
 clue = list("-" * len(win_word))
 null = []
 english_letters = list("qwertyuiopasdfghjklzxcvbnm")
-print(''.join(clue))
-attempts = 0
-while attempts < 9:
-    user_letter = input("Input a letter: ")
-    if user_letter in null:
-        print("You've already guessed this letter")
-    elif user_letter in win_word:
-        if win_word.count(user_letter) >= 2:
-            index = win_word.index(user_letter)
-            clue[index] = user_letter
-            win_word[index] = "-"
-        index = win_word.index(user_letter)
-        clue[index] = user_letter
-        attempts += 1
-    elif len(user_letter) >= 2:
-        print("You should input a single letter")
-    elif user_letter not in english_letters:
-        print("Please enter a lowercase English letter")
-    elif user_letter not in win_word:
-        print("That letter doesn't appear in the word")
-        attempts += 1
 
-    print("\n", ''.join(clue))
-    null.append(user_letter)
 
-if (''.join(clue)) == word:
-    print("You survived!")
-else:
-    print("You lost!")
+def main():
+    user = input("Type 'play' to play the game, 'exit' to quite: ")
+    init(user)
+
+
+def init(user):
+    if user == "play":
+        attempts = 0
+        print(''.join(clue))
+        while attempts < 9:
+            user_letter = input("Input a letter: ")
+            if user_letter in null:
+                print("You've already guessed this letter")
+            elif user_letter in win_word:
+                if win_word.count(user_letter) >= 2:
+                    index = win_word.index(user_letter)
+                    clue[index] = user_letter
+                    win_word[index] = "-"
+                index = win_word.index(user_letter)
+                clue[index] = user_letter
+                attempts += 1
+            elif len(user_letter) >= 2:
+                print("You should input a single letter")
+            elif user_letter not in english_letters:
+                print("Please enter a lowercase English letter")
+            elif user_letter not in win_word:
+                print("That letter doesn't appear in the word")
+                attempts += 1
+
+            print("\n", ''.join(clue))
+            null.append(user_letter)
+
+        if (''.join(clue)) == word:
+            print("You survived!")
+        else:
+            print("You lost!")
+    elif user == "exit":
+        pass
+    else:
+        print("You type incorrect word\n")
+        main()
+
+
+if __name__ == '__main__':
+    main()
