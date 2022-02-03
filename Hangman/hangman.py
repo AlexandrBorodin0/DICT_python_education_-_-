3eefa25444d285587b31fb09459b4a8d75705dfd
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 00:25:32 +0200

    Hangman 7-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,17 +1,19 @@
 import random
 
 
-print("HANGMAN")
+print("HANGMAN\n")
 lists = ["python", "java", "javascript", "php"]
 word = random.choice(lists)
 win_word = list(word)
 clue = list("-" * len(win_word))
 null = []
+english_letters = list("qwertyuiopasdfghjklzxcvbnm")
 print(''.join(clue))
-for i in range(8):
+attempts = 0
+while attempts < 9:
     user_letter = input("Input a letter: ")
     if user_letter in null:
-        print("No improvements")
+        print("You've already guessed this letter")
     elif user_letter in win_word:
         if win_word.count(user_letter) >= 2:
             index = win_word.index(user_letter)
@@ -19,10 +21,18 @@ for i in range(8):
             win_word[index] = "-"
         index = win_word.index(user_letter)
         clue[index] = user_letter
-    else:
+        attempts += 1
+    elif len(user_letter) >= 2:
+        print("You should input a single letter")
+    elif user_letter not in english_letters:
+        print("Please enter a lowercase English letter")
+    elif user_letter not in win_word:
         print("That letter doesn't appear in the word")
-    print(''.join(clue))
+        attempts += 1
+
+    print("\n", ''.join(clue))
     null.append(user_letter)
+
 if (''.join(clue)) == word:
     print("You survived!")
 else:
