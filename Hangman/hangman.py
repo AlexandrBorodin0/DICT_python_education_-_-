23a3e4d5d52bfb4b34b8fd12e70f62fe13a7caff
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 00:25:00 +0200

    Hangman 6-st stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,23 +3,27 @@ import random
 
 print("HANGMAN")
 lists = ["python", "java", "javascript", "php"]
-win_word = list(random.choice(lists))
+word = random.choice(lists)
+win_word = list(word)
 clue = list("-" * len(win_word))
+null = []
 print(''.join(clue))
 for i in range(8):
     user_letter = input("Input a letter: ")
-    if user_letter in win_word:
+    if user_letter in null:
+        print("No improvements")
+    elif user_letter in win_word:
         if win_word.count(user_letter) >= 2:
             index = win_word.index(user_letter)
             clue[index] = user_letter
             win_word[index] = "-"
         index = win_word.index(user_letter)
         clue[index] = user_letter
+    else:
+        print("That letter doesn't appear in the word")
     print(''.join(clue))
-if win_word == clue:
-    pass
+    null.append(user_letter)
+if (''.join(clue)) == word:
+    print("You survived!")
 else:
-    print("That letter doesn't appear in the word")
-
-print("\nThanks or playing!")
-print("We'll se how well you did in next stage")
+    print("You lost!")
