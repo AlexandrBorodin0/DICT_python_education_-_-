b1378f73d01f52680544bd20793ca4bb4e82f205
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 08:00:26 +0200

    DinnerParty 2-st stage

--- DinnerParty/dinnerparty.py
+++ DinnerParty/dinnerparty.py
@@ -1,6 +1,15 @@
 friends_list = []
 
 
+def total(count):
+    try:
+        total_ = int(input("Enter the total amount\n> "))
+        amount = round((total_ / count), 2)
+        return amount
+    except ValueError:
+        print("You should enter numbers!")
+
+
 def main():
     try:
         count = int(input("Enter the number of friends joining (including you):\n> "))
@@ -9,7 +18,8 @@ def main():
                 name_friends = input("Enter the name of every friends (including you), each on a new line:\n> ")
                 friends_list.append(name_friends)
                 count -= 1
-            dict_friends = dict.fromkeys(friends_list, 0)
+            amount = total(len(friends_list))
+            dict_friends = dict.fromkeys(friends_list, amount)
             print(dict_friends)
         else:
             print("No one is joining for the party!")
