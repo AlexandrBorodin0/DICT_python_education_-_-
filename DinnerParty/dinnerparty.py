235d1fbc8802df5908c0fcc63cab80773c25be07
Бородин Александр <crazymegahell064@gmail.com>
2022-02-04 08:00:54 +0200

    DinnerParty 3-st stage

--- DinnerParty/dinnerparty.py
+++ DinnerParty/dinnerparty.py
@@ -1,4 +1,19 @@
+import random
+
+
 friends_list = []
+
+
+def lucky_name(dict_):
+    lucky_ = input("Do you want yo use the 'Who is lucky?' feature? Write Yes/No:\n> ")
+    if lucky_ == "Yes" or lucky_ == "No":
+        if lucky_ == "Yes":
+            dict_lucky = random.choice(list(dict_.keys()))
+            print(f"{dict_lucky} is the lucky one!")
+        else:
+            print("No one is going to be lucky")
+    else:
+        print("You should enter Yes or No!")
 
 
 def total(count):
@@ -13,14 +28,14 @@ def total(count):
 def main():
     try:
         count = int(input("Enter the number of friends joining (including you):\n> "))
-        if count > 0:
+        if 0 < count:
             while count != 0:
                 name_friends = input("Enter the name of every friends (including you), each on a new line:\n> ")
                 friends_list.append(name_friends)
                 count -= 1
             amount = total(len(friends_list))
             dict_friends = dict.fromkeys(friends_list, amount)
-            print(dict_friends)
+            lucky_name(dict_friends)
         else:
             print("No one is joining for the party!")
     except ValueError:
